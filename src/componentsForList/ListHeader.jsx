65c8bf8b5b1c59ddb1ce1a4b49b0c0c9f53d552d
Pradip Mandal <54275322+geekypradip@users.noreply.github.com>
2021-11-15 14:11:32 +0530

    Delete src/componentsForList directory

--- src/componentsForList/ListHeader.jsx
+++
@@ -1,6 +0,0 @@
-function ListHeader({title}) {
-    return (  
-<h2>{title}</h2>
-    );
-}
-export default ListHeader;
--- src/componentsForList/ListItems.jsx
+++
@@ -1,8 +0,0 @@
-import Category from "./CategoryItem";
-function Listitems(props) {
-    return ( 
-        <Category label={props.name} src={props.src}/> 
-     );
-}
-
-export default Listitems;
